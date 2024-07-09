fab8dba137e18371d9828539a4c8194488def32f
AleXComplain24 <alexkorg98@gmail.com>
2024-07-09 23:30:03 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,10 @@
+# Переменные с заданными значениями
+completed_tasks = 12
+hours_spent = 1.5
+course_name = 'Python'
+
+# Вычисление времени на одно задание
+time_per_task = hours_spent / completed_tasks
+
+# Вывод строки с использованием переменных
+print(f'Курс: {course_name}, всего задач: {completed_tasks}, затрачено часов: {hours_spent}, среднее время выполнения {time_per_task:.3f} часа.')
