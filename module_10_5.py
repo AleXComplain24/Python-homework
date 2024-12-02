6db5cd601ce45d92d7389b1c87dc40f9f2367a08
AleXComplain24 <alexkorg98@gmail.com>
2024-12-03 02:04:46 +0300

    Update module_10_5.py

--- module_10_5.py
+++ module_10_5.py
@@ -4,7 +4,7 @@ from multiprocessing import Pool
 # Функция считывания данных из файла
 def read_info(name):
     all_data = [] # Список для данных
-    with open (name, 'r') as file:
+    with open(name, 'r', encoding='utf-8') as file:  # Указана кодировка
         while True:
             line = file.readline()  # Исправлено здесь
             if not line:  # Если строк больше нет
@@ -13,18 +13,17 @@ def read_info(name):
 
 # Основная часть программы
 if __name__ == '__main__':
-    # Список имен файлов
+    # Список имен файлов (например: file 1.txt, file 2.txt и т.д.)
     filenames = [f'./file {number}.txt' for number in range(1, 5)]
-    # Пример файлов file_1.txt, file_2.txt, и т.д.
 
-# Линейный вызов
-start_time = time.time()
-for filename in filenames:
-    read_info(filename)
-print(f"Линейный вызов: {time.time() - start_time:.6f} секунд")
+    # Линейный вызов
+    start_time = time.time()
+    for filename in filenames:
+        read_info(filename)
+    print(f"Линейный вызов: {time.time() - start_time:.6f} секунд")
 
-# Многопроцессный вызов
-start_time = time.time()
-with Pool() as pool:
-    pool.map(read_info, filenames)
-print(f"Многопроцессный вызов: {time.time() - start_time:.6f} секунд")
+    # Многопроцессный вызов
+    start_time = time.time()
+    with Pool() as pool:
+        pool.map(read_info, filenames)
+    print(f"Многопроцессный вызов: {time.time() - start_time:.6f} секунд")
