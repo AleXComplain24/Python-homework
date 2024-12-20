84b5f4064119220cde0fa1c522a5d1e566a383d9
AleXComplain24 <alexkorg98@gmail.com>
2024-12-20 03:48:29 +0300

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -5,20 +5,20 @@ app = FastAPI()
 
 # Маршрут для главной страницы
 @app.get("/")
-def read_main():
-    return {"message": "Главная страница"}
+def read_main() -> str:
+    return "Главная страница"
 
 # Маршрут для страницы администратора
 @app.get("/user/admin")
-def read_admin():
-    return {"message": "Вы вошли как администратор"}
+def read_admin() -> str:
+    return "Вы вошли как администратор"
 
 # Маршрут для страницы пользователя с параметром user_id
 @app.get("/user/{user_id}")
-def read_user(user_id: int):
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
+def read_user(user_id: int) -> str:
+    return f"Вы вошли как пользователь № {user_id}"
 
 # Маршрут для страницы пользователя с передачей параметров через адресную строку
 @app.get("/user")
-def user_info(username: str, age: int):
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+def user_info(username: str, age: int) -> str:
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
